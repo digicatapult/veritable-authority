c4f46e6cf70c7f453c889c460c3e416315d60fdb
Paulius Michelevicius <paulius.michelevicius@gmail.com>
2023-06-08 14:39:35 +0100

    VER-3: update cypress config properties to be inline with v12

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,16 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  testFiles: '**/*.{spec,test}.js',
-  integrationFolder: 'cypress/integration',
-  videoUploadOnPasses: false,
-  requestTimeout: 10000,
-  screenshotOnRunFailure: true,
-  chromeWebSecurity: false,
-  component: {
-    componentFolder: 'src',
-    testFiles: '**/*.{test,spec}.{js,ts,jsx,tsx}',
-    defaultCommandTimeout: 10000,
+  e2e: {
+    supportFile: false,
+    specPattern: 'cypress/integration/*.spec.js',
     baseUrl: 'http://localhost:3000',
   },
-  baseUrl: 'http://localhost:3000',
+
+  component: {
+    devServer: {
+      framework: 'react',
+      bundler: 'webpack',
+    },
+  },
 })
