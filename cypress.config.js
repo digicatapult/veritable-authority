91b1a1ff82a823870d110ff90c16ca39eeb17f95
Paulius Michelevicius <paulius.michelevicius@gmail.com>
2023-06-08 14:26:45 +0100

    VER-3: converting cypress confing into .js

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,17 @@
-{
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
   "testFiles": "**/*.{spec,test}.js",
-  "integrationFolder": "cypress/integration",
-  "videoUploadOnPasses": false,
-  "requestTimeout": 10000,
-  "screenshotOnRunFailure": true,
-  "chromeWebSecurity": false,
-  "component": {
+    "integrationFolder": "cypress/integration",
+      "videoUploadOnPasses": false,
+        "requestTimeout": 10000,
+          "screenshotOnRunFailure": true,
+            "chromeWebSecurity": false,
+              "component": {
     "componentFolder": "src",
-    "testFiles": "**/*.{test,spec}.{js,ts,jsx,tsx}",
-    "defaultCommandTimeout": 10000,
-    "baseUrl": "http://localhost:3000"
+      "testFiles": "**/*.{test,spec}.{js,ts,jsx,tsx}",
+        "defaultCommandTimeout": 10000,
+          "baseUrl": "http://localhost:3000"
   },
   "baseUrl": "http://localhost:3000"
-}
+})
